565babee78c8b3d85c8733ac1d2d90db5d56275c
kiplangat619 <kiplangat.23291@students.kyu.ac.ke>
2024-10-16 20:19:21 +0300

    Add files via upload

---
+++ 1D Array.c
@@ -0,0 +1,13 @@
+// 1D Array
+#include <stdio.h>
+
+int main() {
+    int i;
+    int marks[5] = {50, 90, 60, 70, 40};
+    for(i=0;i<5;i++){
+        printf("marks [%d ] = %d \n",i, marks [i]);
+    }
+    
+
+    return 0;
+}
